757cd0b39243bbb1c2111fdc3bf541a384cd6fca
diegoesteves <nvclts@7>
2018-01-25 13:55:17 +0100

    coding

--- brute_force/brute.py
+++ brute_force/brute.py
@@ -1,6 +1,7 @@
 #import networkx as nx #-> https://networkx.github.io/documentation/networkx-1.7/tutorial/tutorial.html
 import matplotlib.pyplot as plt
 import sys
+import igraph
 from igraph import *
 import os
 os.path.basename(os.path.dirname(os.path.realpath(__file__)))
@@ -36,16 +37,16 @@ def set_style(g):
     # set graph style
     g.es["width"] = 2
     g.vs["color"] = UNTOUCHED_COLOR
-    g.vs["size"] = 50
-    g.vs["label_size"] = 20
+    g.vs["size"] = 30
+    g.vs["label_size"] = 15
     g.vs["label"] = [v.index for v in g.vs]
     g.vs["status"] = [UNTOUCHED_STATUS for v in g.vs]
     layout = g.layout_lgl()
     visual_style = {}
     visual_style["layout"] = layout
-    visual_style["margin"] = [30, 30, 30, 30]
-    visual_style["bbox"] = (1024, 900)
-    visual_style["keep_aspect_ratio"] = True
+    #visual_style["margin"] = [20, 20, 20, 20]
+    #visual_style["bbox"] = (1024, 900)
+    #visual_style["keep_aspect_ratio"] = True
     return visual_style
 
 def to_protect(g, q_risk, q_protected, tot):
@@ -87,55 +88,67 @@ def simulate(g, budget, burns, B_cells):
     budget_left = 0
     budget_current = budget + budget_left
     budget_i = math.floor(budget_current)
-    budget_next_i = budget_current - budget_i
 
     # plot state 0
     plot(g, **visual_style, target=TEMP_PATH_SIMULATION + '/out0.png')
+    #plot(g, target=TEMP_PATH_SIMULATION + '/out0.png')
 
-    to_protect(g, q_risk, q_protected, budget_i)
+
+    if q_risk.empty() is True:
+        raise 'this should not occur'
+    for i in range(budget_i):
+        i = q_risk.get()
+        g.vs[i]["color"] = PROTECTED_COLOR
+        g.vs[i]["status"] = PROTECTED_STATUS
+        q_protected.put(i)
 
     # plot state 1
     plot(g, **visual_style, target=TEMP_PATH_SIMULATION + '/out1.png')
+    exit(0)
+    budget_left = budget_current - budget_i
 
     iter = 2
 
     # update: burn, update risks and protect
 
     while not q_risk.empty():
-        #burns
+
+        # burns and update risks
         for i in range(burns):
             irisk = q_risk.get()
             g.vs[irisk]["color"] = BURNING_COLOR
             g.vs[irisk]["status"] = BURNING_STATUS
             q_burning.put(irisk)
             for i_new_risks in g.neighbors(irisk):
-                q_risk.put(i_new_risks)
-                g.vs[i_new_risks]["color"] = RISK_COLOR
-                g.vs[i_new_risks]["status"] = RISK_STATUS
+                if g.vs[i_new_risks]["status"] == UNTOUCHED_STATUS:
+                    g.vs[i_new_risks]["color"] = RISK_COLOR
+                    g.vs[i_new_risks]["status"] = RISK_STATUS
+                    q_risk.put(i_new_risks)
+        # protect
+        for i in range(budget_i):
+            irisk = q_risk.get()
+            g.vs[irisk]["color"] = PROTECTED_COLOR
+            g.vs[irisk]["status"] = PROTECTED_STATUS
+            q_protected.put(irisk)
 
+        #
+        plot(g, **visual_style, target=TEMP_PATH_SIMULATION + '/out' + str(iter) + '.png')
+        iter +=1
 
+        # update budget
+        budget_current = budget + budget_left
+        budget_i = math.floor(budget_current)
+        budget_left = budget_current - budget_i
 
-
-        for vsn in g.neighbors(vs):
-            s = g.vs[vsn]["status"]
-            if s == UNTOUCHED_STATUS and i_budget_state <= accumulated_budget:
-                g.vs[vsn]["color"]  = PROTECTED_COLOR
-                g.vs[vsn]["status"] = PROTECTED_STATUS
-                i_budget_state += 1
-
-            if s == UNTOUCHED_STATUS and i_burned_state <= nr_vb:
-                g.vs[vsn]["color"]  = BURNING_COLOR
-                g.vs[vsn]["status"] = BURNING_STATUS
-                i_burned_state += 1
-                qv_burning.put(vsn) # next iteration should visit its neighbors
 
 
 
 def main(argv):
     # simulation parameters
-    nr_edges = 2
-    nr_vertex = 10
-    g = Graph.Tree(nr_vertex, nr_edges)
+    v = 4
+    g = Graph.Lattice([v,v], nei=1, directed=False, mutual=True, circular=False)
+    g.layout_grid(0, 0, dim=2)
+
 
     budget = 1.6
     burns = 2
